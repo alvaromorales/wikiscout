a94a40505bd8e23841c24c9f1158b291005b640b
Alvaro Morales <alvarom@mit.edu>
2013-11-20 02:16:23 -0500

    Debugged infobox tools and added tests

--- tests/test_infobox.py
+++ tests/test_infobox.py
@@ -33,3 +33,24 @@ class TestValidationRules(unittest.TestCase):
 
     def test_invalid_template(self):
         assert infobox.validate_template('infobox_person/template') == False
+
+class TextInfoboxParsing(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.article = {"title":"Bill Clinton","wikiTitle":"Bill_Clinton","wid":5424,"lang":"en","namespace":"","integerNamespace":0,"timestamp":"2013-10-21T13:51:33Z","type":"ARTICLE","paragraphs":["William Jefferson Clinton (born on August 19, 1946 as William Jefferson Blythe III), better known as Bill Clinton, is an American lawyer who was the 42nd President of the United States.  He served from 1993 to 2001.","Clinton married Hillary Rodham in 1975. In 1980, their daughter, Chelsea Clinton, was born.","TEMPLATE[wikiquote, Bill Clinton] TEMPLATE[commons, Bill Clinton]"],"highlights":["William Jefferson Clinton","William Jefferson Blythe III","Bill Clinton","My Life"],"infobox":{"name":"Infobox_officeholder","description":["name          = Bill Clinton","vicepresident = [[Al Gore]]","term_start    = January 20, 1993", "1    = invalid attribute", "no_value    = "]}}
+
+    def test_parse(self):
+        infobox.parse(self.article['infobox']['name'], self.article['infobox']['description'])
+        
+    def test_get_attributes(self):
+        article_infobox = infobox.parse(self.article['infobox']['name'], self.article['infobox']['description'])
+        self.assertItemsEqual(infobox.get_attributes(article_infobox),['name','vicepresident','term_start','no_value'])
+
+    def test_get_all_attributes(self):
+        article_infobox = infobox.parse(self.article['infobox']['name'], self.article['infobox']['description'])
+        self.assertItemsEqual(infobox.get_all_attributes(article_infobox),['name','vicepresident','term_start','no_value','1'])
+
+    def test_get_items(self):
+        article_infobox = infobox.parse(self.article['infobox']['name'], self.article['infobox']['description'])
+        self.assertItemsEqual(infobox.get_items(article_infobox), [('name','Bill Clinton'), ('vicepresident','Al Gore'), ('term_start', 'January 20, 1993')])
