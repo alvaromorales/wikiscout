da1ae68a0b627df3cb99759de82df4bd6de5b90a
Alvaro Morales <alvarom@mit.edu>
2013-11-20 04:09:33 -0500

    Added extractor and sentence_tools. Updated tests

---
+++ scout/extractor.py
@@ -0,0 +1,112 @@
+import re
+import sys
+import json
+
+import mrjob.util
+from mrjob.protocol import JSONValueProtocol
+from mrjob.job import MRJob
+
+from tools import infobox
+from tools import sentence_tools
+
+class MRExtractor(MRJob):
+    INPUT_PROTOCOL = JSONValueProtocol
+    OUTPUT_PROTOCOL = JSONValueProtocol
+
+    def configure_options(self):
+        super(MRExtractor, self).configure_options()
+        self.add_file_option('--attributes')
+
+    def mapper_init(self):
+        f = open(self.options.attributes)
+        top_attributes = {}
+        for l in f:
+            template_attrs = json.loads(l.strip())
+            top_attributes[template_attrs['template']] = template_attrs['attributes']
+            
+        self.top_attributes = top_attributes
+
+    def mapper(self, key, joined_article):
+        title = joined_article['title']
+        en_article = joined_article['en']
+        simple_article = joined_article['simple']
+
+        if 'infobox' not in en_article:
+            return
+
+        article_infobox = infobox.parse(en_article['infobox']['name'], en_article['infobox']['description'])
+
+        if article_infobox is None:
+            return
+        
+        infobox_type = infobox.normalize_template(article_infobox.name)
+
+        if infobox_type not in self.top_attributes:
+            return
+        
+        matching_attributes = {}
+        
+        for attribute, value in infobox.get_items(article_infobox):
+            if attribute in self.top_attributes[infobox_type]:
+                matching_attributes[attribute] = value
+
+        simple_sentences = sentence_tools.get(simple_article['paragraphs'])
+        candidate_sentences = {}
+
+        for attribute,value in matching_attributes.items():
+            attribute_matches = []
+            if not infobox.validate_value(value):
+                continue
+
+            for s in simple_sentences:
+                if s.find(value) != -1:
+                    attribute_matches.append({'sentence':s,'value':value,'title':title})
+
+            if attribute not in candidate_sentences:
+                candidate_sentences[attribute] = []
+            
+            map(lambda c: candidate_sentences[attribute].append(c), attribute_matches)
+
+        for attribute,candidates in candidate_sentences.items():
+            yield (attribute,infobox_type), candidates
+
+    def reducer_attributes(self, attribute_infobox, candidates):
+        attribute, infobox_type = attribute_infobox
+
+        candidate_sentences = {
+            'attribute' : attribute,
+            'sentences' : []
+            }
+        
+        for candidate_group in candidates:
+            for c in candidate_group:
+                candidate_sentences['sentences'].append(c)
+
+        if len(candidate_sentences['sentences']) > 1:
+            yield infobox_type, candidate_sentences
+
+    def mapper_infobox_type(self,infobox_type,candidates):
+        yield infobox_type,candidates
+
+    def reducer_infobox_type(self,infobox_type,candidates):
+        infobox_type_candidates = {
+            'infobox_type': infobox_type,
+            'attributes' : {}
+            }
+        
+        for c in candidates:
+            infobox_type_candidates['attributes'][c['attribute']] = c['sentences']
+
+        yield None, infobox_type_candidates
+        
+    def steps(self):
+        return [
+            self.mr(mapper_init=self.mapper_init,
+                    mapper=self.mapper,
+                    reducer=self.reducer_attributes),
+            self.mr(mapper=self.mapper_infobox_type,
+                    reducer=self.reducer_infobox_type)
+        ]
+
+if __name__ == '__main__':
+    MRExtractor.run()
--- tests/test_sentence_tools.py
+++ tests/test_sentence_tools.py
@@ -15,6 +15,9 @@ class TestNormalize(unittest.TestCase):
     def test_normalize_template(self):
         self.assertEqual(sentence_tools.normalize('In 2013, Clinton started following Buddhist meditation to help him relax.TEMPLATE[cite_web, url=http://www.dailymail.co.uk/news/article-2183101/Bill-Clinton-turns-art-Buddhist-meditation-relax.html, title=Chill Bill: Clinton turns to the art of Buddhist meditation to relax, accessdate=March 6, 2013]'), 'In 2013, Clinton started following Buddhist meditation to help him relax.')
 
+    def test_normalize_ref_template(self):
+        self.assertEqual(sentence_tools.normalize('TEMPLATE[refimprove, date=March 2011]   Michael Crichton (October 23, 1942 - November 4, 2008) was an American author of many books.'),'Michael Crichton (October 23, 1942 - November 4, 2008) was an American author of many books.')
+
     def test_no_change(self):
         self.assertEqual(sentence_tools.normalize('In 1994, during Clinton\'s first term in office, the Congress switched to a Republican majority.'),'In 1994, during Clinton\'s first term in office, the Congress switched to a Republican majority.')
 
