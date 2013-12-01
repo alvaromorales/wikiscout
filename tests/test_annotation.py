227cb35472958917940b74044977c5207c920a02
Alvaro Morales <alvarom@mit.edu>
2013-12-01 16:59:08 -0500

    Added wrapper to MongoDB database with Wikipedia dumps. Updated tests

--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -17,96 +17,106 @@ class TestGetClass(unittest.TestCase):
 class TestSubTitle(unittest.TestCase):
     def test_simple(self):
         sentence = "Bill Clinton married Hillary Clinton in 1975."
-        title = "Bill Clinton"
+        wiki_title = "Bill_Clinton"
         template = "infobox_officeholder"
-        
-        annotation = Annotation(sentence,title,template)
+        value = "Hillary Clinton"
+
+        annotation = Annotation(sentence,wiki_title,template,value)
 
         self.assertEqual(annotation.sub_title(),"any-wikipedia-officeholder married Hillary Clinton in 1975.")
 
     def test_possesive(self):
         sentence = "Bill Clinton's wife is Hillary Clinton."
-        title = "Bill Clinton"
+        wiki_title = "Bill_Clinton"
         template = "infobox_officeholder"
-        
-        annotation = Annotation(sentence,title,template)
+        value = "Hillary Clinton"
+
+        annotation = Annotation(sentence,wiki_title,template,value)
 
         self.assertEqual(annotation.sub_title(),"any-wikipedia-officeholder's wife is Hillary Clinton.")
     
     def test_no_title(self):
         sentence = "Hillary Rodham Clinton was born in Chicago."
-        title = "Bill Clinton"
+        wiki_title = "Bill_Clinton"
         template = "infobox_officeholder"
-        
-        annotation = Annotation(sentence,title,template)
+        value = "Chicago"
+
+        annotation = Annotation(sentence,wiki_title,template,value)
         
         self.assertIsNone(annotation.sub_title())
 
 class TestSubPronoun(unittest.TestCase):
     def test_possesive(self):
         sentence = "He was born in Arkansas."
-        title = "Bill Clinton"
+        wiki_title = "Bill_Clinton"
         template = "infobox_officeholder"
+        value = "Arkansas"
         
-        annotation = Annotation(sentence,title,template)
+        annotation = Annotation(sentence,wiki_title,template,value)
 
         self.assertEquals(annotation.sub_pronoun(),"any-wikipedia-officeholder was born in Arkansas.")
 
     def test_subjective(self):
         sentence = "His father was William Jefferson Blythe, Jr."
-        title = "Bill Clinton"
+        wiki_title = "Bill_Clinton"
         template = "infobox_officeholder"
-        
-        annotation = Annotation(sentence,title,template)
+        value = "William Jefferson Blythe, Jr"
+
+        annotation = Annotation(sentence,wiki_title,template,value)
 
         self.assertEquals(annotation.sub_pronoun(),"any-wikipedia-officeholder's father was William Jefferson Blythe, Jr.")
         
     def test_no_pronoun(self):
         sentence = "Georgetown was his alma mater."
-        title = "Bill Clinton"
+        wiki_title = "Bill_Clinton"
         template = "infobox_officeholder"
+        value = "Georgetown"
         
-        annotation = Annotation(sentence,title,template)
+        annotation = Annotation(sentence,wiki_title,template,value)
 
         self.assertIsNone(annotation.sub_pronoun())
     
 class TestSubHighlights(unittest.TestCase):
     def test_longest_highlight(self):
         sentence = "William Jefferson Clinton was a US president."
-        title = "Bill Clinton"
+        wiki_title = "Bill_Clinton"
         template = "infobox_officeholder"
+        value = "US president"
         highlights = ['William Jefferson Clinton', 'William Jefferson Blythe III', 'Bill Clinton', 'My Life','Bill','Clinton']
 
-        annotation = Annotation(sentence,title,template,highlights=highlights)
+        annotation = Annotation(sentence,wiki_title,template,value,highlights=highlights)
         
         self.assertEquals(annotation.sub_highlights(),"any-wikipedia-officeholder was a US president.")
 
     def test_possesive(self):
         sentence = "Clinton's nickname is Bill."
-        title = "Bill Clinton"
+        wiki_title = "Bill_Clinton"
         template = "infobox_officeholder"
+        value = "Bill"
         highlights = ['William Jefferson Clinton', 'William Jefferson Blythe III', 'Bill Clinton', 'My Life','Bill','Clinton']
 
-        annotation = Annotation(sentence,title,template,highlights=highlights)
+        annotation = Annotation(sentence,wiki_title,template,value,highlights=highlights)
         
         self.assertEquals(annotation.sub_highlights(),"any-wikipedia-officeholder's nickname is Bill.")
         
     def test_no_highlights(self):
         sentence = "Barack Obama is the president of the US."
-        title = "Bill Clinton"
+        wiki_title = "Bill_Clinton"
         template = "infobox_officeholder"
+        value = "president of the US"
         highlights = ['William Jefferson Clinton', 'William Jefferson Blythe III', 'Bill Clinton', 'My Life','Bill','Clinton']
 
-        annotation = Annotation(sentence,title,template,highlights=highlights)
+        annotation = Annotation(sentence,wiki_title,template,value,highlights=highlights)
         
         self.assertIsNone(annotation.sub_highlights())
 
 class TestSubWikipediaBaseSymbols(unittest.TestCase):
     def test(self):
         sentence = "William Clinton was the president of the US."
-        title = "Bill Clinton"
+        wiki_title = "Bill_Clinton"
         template = "infobox_officeholder"
+        value = "president of the US"
         
-        annotation = Annotation(sentence,title,template)        
+        annotation = Annotation(sentence,wiki_title,template,value)        
         self.assertEquals(annotation.sub_wb_subject(),"any-wikipedia-officeholder was the president of the US.")
 
