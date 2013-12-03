3c59750eedbe800395d1ea3d216e160b9d3f2aeb
Alvaro Morales <alvarom@mit.edu>
2013-12-03 03:43:05 -0500

    Added NER substitution to annotations

--- scout/tools/annotation.py
+++ scout/tools/annotation.py
@@ -2,6 +2,7 @@ import re
 from wikipediabase import WikipediaBase
 from wikidump import WikiDump
 from omnibase import Omnibase
+from corenlp import CoreNLP
 from nltk.corpus import stopwords
 import logging
 
@@ -20,11 +21,12 @@ class Annotation:
         self.wb = WikipediaBase(host='nauru.csail.mit.edu')
         self.ob = Omnibase(host='nauru.csail.mit.edu')
         self.wdump = WikiDump(host='nauru.csail.mit.edu')
+        self.corenlp = CoreNLP()
         
         self.get_article_data()
 
         self.highlights.extend(highlights)
-        self.highlights = sorted(self.highlights,key=len,reverse=True)
+        self.highlights = sorted(filter(lambda h: h != '',self.highlights),key=len,reverse=True)
         
         self.annotation = None
 
@@ -58,12 +60,12 @@ class Annotation:
             # spaces are invalid
             return
         
-        if re.match(r'^infobox_',s,flags=re.IGNORECASE):
+        if re.search(r'^infobox_',s,flags=re.IGNORECASE):
             symbol = re.sub(r'^infobox_','any-wikipedia-',s,flags=re.IGNORECASE)
             symbol = symbol.lower().replace('_','-')
             return symbol
 
-        if re.match(r'^wikipedia-',s,flags=re.IGNORECASE):
+        if re.search(r'^wikipedia-',s,flags=re.IGNORECASE):
             symbol = re.sub(r'^wikipedia-','any-wikipedia-',s,flags=re.IGNORECASE)
             symbol = symbol.lower().replace('_','-')
             return symbol
@@ -73,34 +75,34 @@ class Annotation:
 
     def sub_title(self):
         # possesive
-        if re.match(r'\b%s\'s\b'%re.escape(self.title),self.sentence,flags=re.IGNORECASE):
+        if re.search(r'\b%s\'s\b'%re.escape(self.title),self.sentence,flags=re.IGNORECASE):
             return re.sub(r'\b%s\'s\b'%re.escape(self.title),'%s\'s'%self.symbol,self.sentence,flags=re.IGNORECASE)
         
         # subjective
-        if re.match(r'\b%s\b'%re.escape(self.title),self.sentence,flags=re.IGNORECASE):
+        if re.search(r'\b%s\b'%re.escape(self.title),self.sentence,flags=re.IGNORECASE):
             return re.sub(r'\b%s\b'%re.escape(self.title),'%s'%self.symbol,self.sentence,flags=re.IGNORECASE)
 
     def sub_pronoun(self):
         # possesive
-        if re.match(r'^(?:His|Her|Its)\b',self.sentence):
-            return re.sub(r'^(?:His|Her|Its)\b','%s\'s'%self.symbol,self.sentence)
+        if re.search(r'^(?:His|Her|Its|Their)\b',self.sentence):
+            return re.sub(r'^(?:His|Her|Its|Their)\b','%s\'s'%self.symbol,self.sentence)
         
         # subjective
-        if re.match(r'^(?:He|She|It|They)\b',self.sentence):
+        if re.search(r'^(?:He|She|It|They)\b',self.sentence):
             return re.sub(r'^(?:He|She|It|They)\b',self.symbol,self.sentence)
         
     def sub_highlights(self):
         for h in self.highlights:
-            if re.match(r'\b%s(?:\'s)?\b'%re.escape(h),self.sentence):
+            if re.search(r'\b%s(?:\'s)?\b'%re.escape(h),self.sentence):
                 return re.sub(r'\b%s'%re.escape(h),self.symbol,self.sentence,count=1)
 
     @staticmethod
     def get_class(classes):
-        ignore = set(['wikipedia-term','wikipedia-paragraphs','wikipedia-paragraph','wikipedia-person'])
+        ignore = set(['wikipedia-term','wikipedia-paragraphs','wikipedia-paragraph'])
         common = set(['wikipedia-person'])
-
+        
         classes = list(set(classes))
-
+        
         if len(classes) == 0:
             return None
 
@@ -112,23 +114,25 @@ class Annotation:
 
         classes = filter(lambda c: c not in ignore,classes)
         if len(classes) == 0:
-            print "No classes left after 'ignore' filter"
-            raise
+            raise Exception("No classes left after 'ignore' filter")
 
         if len(classes) == 1:
             return classes[0]
 
         classes = filter(lambda c: c not in common,classes)
-        if len(classes) == 1:
-            return classes[0]
-
-        base = re.sub(r'^any-wikipedia-','',self.symbol)
-        for c in classes:
-            if re.sub(r'^wikipedia-','',c) == base:
-                return c
+        # shortest matching symbol?
+        classes = sorted(classes,key=len)
         
-        print "No classes left after 'common' filter"
-        raise
+        if len(classes) > 0:
+            print classes
+            return classes[0]
+        
+        #base = re.sub(r'^any-wikipedia-','',symbol)
+        #for c in classes:
+        #    if re.sub(r'^wikipedia-','',c) == base:
+        #        return c
+        
+        raise Exception("No classes left after 'common' filter")
 
     def sub_wb_subject(self):
         # try to find symbols with the same title
@@ -139,44 +143,39 @@ class Annotation:
             match = longest['match']
             
             # possesive
-            if re.match(r'\b%s\'s\b'%re.escape(match),self.sentence):
+            if re.search(r'\b%s\'s\b'%re.escape(match),self.sentence):
                 return re.sub(r'\b%s\'s\b'%re.escape(match),'%s\'s'%self.symbol,self.sentence)
             # subjective
-            if re.match(r'\b%s\b'%re.escape(match),self.sentence):
+            if re.search(r'\b%s\b'%re.escape(match),self.sentence):
                 return re.sub(r'\b%s\b'%re.escape(match),'%s'%self.symbol,self.sentence)
             
         # special case: infobox-person
         if 'wikipedia-person' in self.wb_classes:
             synonyms = filter(lambda s: s is not None and s != '', self.ob.get('nobel-person',self.title,'SYNONYMS'))
-            for s in synonyms:
+            
+            for syn in synonyms:
                 # possesive
-                if re.match(r'\b%s\'s\b'%re.escape(s),self.sentence):
-                    return re.sub(r'\b%s\'s\b'%re.escape(s),'%s\'s'%self.symbol,self.sentence)
+                if re.search(r'\b%s\'s\b'%re.escape(syn),self.sentence):
+                    return re.sub(r'\b%s\'s\b'%re.escape(syn),'%s\'s'%self.symbol,self.sentence)
+
                 # subjective
-                if re.match(r'\b%s\b'%re.escape(s),self.sentence):
-                    return re.sub(r'\b%s\b'%re.escape(s),'%s'%self.symbol,self.sentence)
+                if re.search(r'\b%s\b'%re.escape(syn),self.sentence):
+                    return re.sub(r'\b%s\b'%re.escape(syn),'%s'%self.symbol,self.sentence)
 
         # try to find an omnibase symbol with the same class as the title
         c = self.get_class(self.wb_classes)
-        
-        print "Sentence: %s"%self.sentence
-        print "Class: %s"%c
-        print "WB: %s"%self.wb_classes
-        print "OB: %s"%str(self.filter_symbols(self.ob_symbols))
-        
-        raise Exception("Reached else clause in WB subject")
+        print "FIND SUBJECT FROM CLASSES: %s"%c
 
     # assumes annotation contains substring value
-    def replace_value(self,annotation,value,wiki_title):
-        wiki_title = self.wdump.get_title_template(wiki_title)
+    def replace_value(self,annotation,value,wiki_title,prefer_wikibase=True):
+        title_template = self.wdump.get_title_template(wiki_title)
         
-        if wiki_title is None:
+        if title_template is None:
             return
 
-        title,template = wiki_title
+        title,template = title_template
 
-        if template is not None:
-            title,template = wiki_title
+        if template is not None and not prefer_wikibase:
             return annotation.replace(value,self.generate_symbol(template))
         else:
             c = self.get_class(self.wb.get_classes(title))
@@ -205,14 +204,16 @@ class Annotation:
     def sub_subject(self):
         for f in [self.sub_title,self.sub_pronoun,self.sub_highlights,self.sub_wb_subject]:
             annotation = f()
-
+            
             if annotation is not None:
                 return annotation
+
+        raise Exception("Could not find subject %s in the annotation."%self.title)
 
     def sub_value(self):
         # attempt to lookup and replace value
         value_symbols = self.ob.get_known(self.value,omnibase_class='wikipedia-term')
-        value_matches = sorted([symbol for symbol in value_symbols if symbol['symbol'] == self.title],key= lambda symbol: symbol['span'][1] - symbol['span'][0],reverse=True)
+        value_matches = sorted([symbol for symbol in value_symbols if symbol['symbol'] == self.value],key= lambda symbol: symbol['span'][1] - symbol['span'][0],reverse=True)
         
         if len(value_matches) > 0:
             longest = value_matches[0]
@@ -229,20 +230,36 @@ class Annotation:
         print "Could not substitute value = \"%s\". Used wikipedia-term."%(self.value)
         self.annotation = self.annotation.replace(self.value,'any-wikipedia-term')
 
-    def get(self):
-        # subject substitution
-        annotation = self.sub_subject()
-        
-        if annotation is None:
-            print "Could not find a subject in \"%s\"."%self.sentence
-            raise
+    def sub_ner(self):
+        annotation = self.annotation
+        annotation = self.corenlp.ner(annotation)
         self.annotation = annotation
 
-        # link substitution
+    def normalize(self):
+        annotation = self.annotation
+
+        annotation = re.sub(r'\(.*?\)','',annotation)
+        annotation = re.sub(r'\s+',' ',annotation)
+
+        self.annotation = annotation
+
+    def get(self):
+        # subject substitution
+        try:
+            annotation = self.sub_subject()
+        except Exception as e:
+            print e
+            return None
+        
+        if annotation is None:
+            raise Exception("Could not find a subject in: \"%s\"."%self.sentence)
+        self.annotation = annotation
+        
+        self.sub_value()
         self.sub_links()
 
-        # value substitution
-        if self.annotation.find(self.value) != -1:
-            self.sub_value()
+        self.normalize()
+        self.sub_ner()
         
+        self.normalize()
         return self.annotation
