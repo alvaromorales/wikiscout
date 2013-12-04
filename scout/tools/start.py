a742524c24f5138cf9256e3c855e62f9419fbdd3
Alvaro Morales <alvarom@mit.edu>
2013-12-04 17:07:09 -0500

    Added schemata and updated the START wrapper for parsing

--- scout/tools/start.py
+++ scout/tools/start.py
@@ -14,12 +14,31 @@ class START:
             self.telnet = telnetlib.Telnet(self.host,8002)
             self.remote_address = socket.gethostbyname(socket.gethostname())
         except:
-            print "Could not connect to START host %s"%self.host
-            raise
+            raise Exception("Could not connect to START host %s"%self.host)
     
-    def format_request(self,query,action):
-        request = '(:REMOTE-ADDRESS "%s" :VB "normal" :QUERY "%s" :USER_AGENT "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36" :TE "XML" :KB "yes" :REFERRER "http://nauru.csail.mit.edu/paraphrase-test" :DE "no" :ACTION "%s" :FG "yes" :SERVER "ailab" :QE "HTML" :EQ "history" :QI "qsymbols" :MACHINE "nauru.csail.mit.edu" :HTTP_HOST "groups.csail.mit.edu")'%(self.remote_address,query,action)
+    def format_request(self,query,action,kb='yes',machine='nauru.csail.mit.edu',server='ailab'):
+        request = '(:REMOTE-ADDRESS "%s" :VB "normal" :QUERY "%s" :USER_AGENT "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36" :TE "XML" :KB "%s" :REFERRER "http://nauru.csail.mit.edu/wikiscout" :DE "no" :ACTION "%s" :FG "yes" :SERVER "%s" :QE "HTML" :EQ "history" :QI "qsymbols" :MACHINE "%s" :HTTP_HOST "groups.csail.mit.edu")'%(self.remote_address,query,kb,action,machine,server)
         return request
+
+    def parseable(self,sentence):
+        return self.parse(sentence) is not None
+    
+    def parse(self,sentence):
+        self.connect()
+        request = self.format_request(sentence,'parse',kb='no')
+        self.telnet.write(request)
+        response = self.telnet.read_all().strip()
+
+        try:
+            parsed = xmltodict.parse(response)
+        except:
+            raise Exception("Could not parse:\n%s"%response)
+
+        if 'interaction' in parsed and 'query' in parsed['interaction']:
+            if 'exception' not in parsed['interaction']['query']:
+                return parsed['interaction']['query']
+        
+        return None
 
     def ask(self,question):
         self.connect()
