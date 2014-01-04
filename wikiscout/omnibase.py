88eb8068a59299753d184509930491462bf79623
Alvaro Morales <alvarom@mit.edu>
2014-01-03 19:28:28 -0500

    Refactored START services into modules

--- wikiscout/omnibase.py
+++ wikiscout/omnibase.py
@@ -3,99 +3,97 @@ import re
 from pyparsing import OneOrMore, nestedExpr
 import telnetlib
 
-class Omnibase:
-    def __init__(self,host='localhost'):
-        self.host = host
-        self.scheme_parser = OneOrMore(nestedExpr())
-        self.connect()
-    
-    def connect(self):
-        # open a new telnet connection to omnibase
-        try:
-            self.telnet = telnetlib.Telnet(self.host,8008)
-        except:
-            print "Could not connect to Omnibase host %s"%self.host
-            raise
-    
-    def get(self,omnibase_class,symbol,attribute):
-        self.connect()
-        
-        self.telnet.write('(get "%s" "%s"  "%s")'%(omnibase_class,symbol,attribute.upper()))
-        
-        response = self.telnet.read_all()
-        response = re.sub(r'\n','',response)
 
-        self.close()
+class OmnibaseConnectionException(Exception):
+    pass
 
-        if response.strip() == '#f':
-            return None
 
-        return list(eval(response.replace('" "','","')))
+class OmnibaseParseException(Exception):
+    pass
 
-    def get_symbols(self,s,omnibase_class=None):
-        self.connect()
-        s = json.dumps(s)
 
-        if omnibase_class is not None:
-            self.telnet.write('(get-symbols %s \':class "%s")'%(s,omnibase_class))
-        else:
-            self.telnet.write('(get-symbols %s)'%s)
+class OmnibaseException(Exception):
+    pass
 
-        response = self.telnet.read_all()
 
-        try:
-            parsed_response = self.parse_get_symbols_response(response)
-        except:
-            raise Exception('Omnibase could not parse: %s'%self.sentence)
+def _connect(host='localhost'):
+    try:
+        conn = telnetlib.Telnet(host, 8008)
+        return conn
+    except:
+        raise OmnibaseConnectionException('Could not connect to Omnibase host %s' % host)
 
-        self.close()
-        return parsed_response
 
-    def get_known(self,s,omnibase_class=None):
-        self.connect()
-        s = json.dumps(s)
+def get(cls, symbol, attribute, host='localhost'):
+    conn = _connect(host)
+    conn.write('(get "%s" "%s"  "%s")' % (cls, symbol, attribute.upper()))
+    response = conn.read_all().strip()
+    conn.close()
+    response = re.sub(r'\n', '', response)
 
-        if omnibase_class is not None:
-            self.telnet.write('(get-known %s \':class "%s")'%(s,omnibase_class))
-        else:
-            self.telnet.write('(get-known %s)'%s)
-        
-        response = self.telnet.read_all()
-        parsed_response = self.parse_get_symbols_response(response)
-        
-        self.close()
-        return parsed_response
+    if response == '#f':
+        return None
 
-    def parse_get_symbols_response(self,response):
-        if response[:2] == '()':
-            return None
+    return list(eval(response.replace('" "', '","')))
 
-        # some responses are HUGE ... we truncate them
-        
-        if len(response) > 0 and response[-2:] != "))":
-            last_occurence = response.rfind(':priority 0)')
-            response = response[:last_occurence + len(':priority 0)')] + ")"
-        
-        parsed_response = []
 
-        try:
-            data = self.scheme_parser.parseString(response)
-        except:
-            raise Exception("Could not parse %s"%response)
+def get_symbols(s, cls=None, host='localhost'):
+    conn = _connect(host)
+    s = json.dumps(s)
 
-        if data[0][0] == "error":
-            print response
-            raise Exception()
-        
-        for d in data[0]:
-            r = {}
-            r['class'] = d[0]
-            r['match'] = d[1].replace('"', '')
-            r['span'] = (int(d[3][0]), int(d[3][1]))
-            r['symbol'] = d[5].replace('"', '')
-            parsed_response.append(r)
-        return parsed_response
+    if cls:
+        conn.write('(get-symbols %s \':class "%s")' % (s, cls))
+    else:
+        conn.write('(get-symbols %s)' % s)
 
-    def close(self):
-        self.telnet.close()
+    response = conn.read_all()
+    conn.close()
 
+    parsed_response = _parse_get_symbols_response(response)
+    return parsed_response
+
+
+def get_known(s, cls=None, host='localhost'):
+    conn = _connect(host)
+    s = json.dumps(s)
+
+    if cls:
+        conn.write('(get-known %s \':class "%s")' % (s, cls))
+    else:
+        conn.write('(get-known %s)' % s)
+
+    response = conn.read_all()
+    conn.close()
+
+    parsed_response = _parse_get_symbols_response(response)
+    return parsed_response
+
+
+def _parse_get_symbols_response(response):
+    if response[:2] == '()':
+        return None
+
+    # some responses are HUGE ... we truncate them
+    if len(response) > 0 and response[-2:] != "))":
+        last_occurence = response.rfind(':priority 0)')
+        response = response[:last_occurence + len(':priority 0)')] + ")"
+
+    parsed_response = []
+
+    try:
+        data = OneOrMore(nestedExpr()).parseString(response)
+    except:
+        raise OmnibaseParseException("Could not parse %s" % response)
+
+    if data[0][0] == "error":
+        print response
+        raise Exception()
+
+    for d in data[0]:
+        r = {}
+        r['class'] = d[0]
+        r['match'] = d[1].replace('"', '')
+        r['span'] = (int(d[3][0]), int(d[3][1]))
+        r['symbol'] = d[5].replace('"', '')
+        parsed_response.append(r)
+    return parsed_response
