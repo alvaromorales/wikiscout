79535e4781538933ab1768576d3f2820a5261ff3
Alvaro Morales <alvarom@mit.edu>
2014-04-21 13:55:24 -0400

    Improve sentence normalization

--- wikiscout/sentence.py
+++ wikiscout/sentence.py
@@ -28,22 +28,73 @@ def validate(sentence):
         re.match('TEMPLATE\[[iI]nfobox', sentence) is None
 
 
-def normalize(sentence):
-    sentence = unicode(sentence)
-    sentence = re.sub('(?i)TEMPLATE\[.*?\]','',sentence)
-    sentence = unidecode(sentence)
-    sentence = sentence.strip()
-    return sentence
+def normalize(text):
+    text = unicode(text)
+    text = re.sub(r'(\S)http://', r'\1 http://', text) # whitespaces before hyperlinks
+    text = re.sub(r'http://[^\s]+', ' ', text)    # hyperlinks
+    text = re.sub(r'\([^)]*\)', '', text)       # brackets, removed by START anyway
+    text = re.sub(r'(?i)TEMPLATE\[.*?\]','', text)  # template tags
+    text = re.sub('&nbsp;', ' ', text)              # common html entity
+    text = re.sub(r'\s+', ' ', text)                # multiple whitespace
+    text = re.sub(r'([a-zA-Z0-9"\'])[.]([a-zA-Z]{2,})', r'\1. \2', text) # sentences with no <period> <whitespace>
+                                                                         # does not match acronyms like U.S.
+    text = text.strip()
+    if not text.endswith('.'):
+        text += '.'
+    text = unidecode(text)
+    return text
 
 
-def get(paragraphs):
+def contains_object(sentence, title, gender, synonyms):
+    s = sentence.lower()
+    title = title.lower()
+
+    if re.search(r'\b%s\b' % title, s) or re.search(r'\b%s\'s\b' % title, s):
+        return True
+
+    synonyms = [syn.lower() for syn in synonyms]
+    for syn in synonyms:
+        if re.search(r'\b%s\b' % re.escape(syn), s) or re.search(r'\b%s\'s\b' % re.escape(syn), s):
+            return True
+
+    if gender == 'masculine':
+        if re.search(r'\bhe\b', s) or re.search(r'\bhis\b', s) or re.search(r'\bhim\b', s):
+            return True
+
+    if gender == 'femenine':
+        if re.search(r'\bshe\b', s) or re.search(r'\bher\b', s) or re.search(r'\bhers\b', s):
+            return True
+
+    if gender == 'neuter':
+        if sentence.startswith('It '):
+            return True
+
+    return False
+
+
+def get(paragraphs, title, gender, synonyms):
     """Gets valid sentences from paragraphs."""
-
     paragraphs = filter(lambda p: validate(p), paragraphs)
     paragraphs = [normalize(p) for p in paragraphs]
 
     text = " ".join(paragraphs)
     sentences = sent_tokenize(text)
     sentences = filter(lambda s: s.find('=') == -1, sentences)
+    sentences = filter(lambda s: contains_object(s, title, gender, synonyms), sentences)
 
     return sentences
+
+
+def get_gender(paragraphs):
+    text = " ".join(paragraphs).lower()
+
+    masculine = len(re.findall(r'\bhe\b', text)) + len(re.findall(r'\bhis\b', text)) + len(re.findall(r'\bhim\b', text))
+    femenine = len(re.findall(r'\bshe\b', text)) + len(re.findall(r'\bher\b', text)) + len(re.findall(r'\bhers\b', text))
+    neuter = len(re.findall(r'\bit\b', text)) + len(re.findall(r'\bits\b', text))
+
+    if neuter > masculine and neuter > femenine:
+        return 'neuter'
+    elif masculine >= femenine:
+        return 'masculine'
+    else:
+        return 'femenine'
