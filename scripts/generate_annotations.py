9de71ebbedd1c032e81d62a96916b906ffe627e9
alvarom <alvarom@f1b2aeda-bd07-0410-ad1a-e603d8ac5cf8>
2014-06-27 03:58:28 +0000

    Change generation script to use a pool of workers
    git-svn-id: svn+ssh://svn.csail.mit.edu/afs/csail.mit.edu/group/infolab/svnroot/wikiscout@1901 f1b2aeda-bd07-0410-ad1a-e603d8ac5cf8

--- scripts/generate_annotations.py
+++ scripts/generate_annotations.py
@@ -1,143 +1,111 @@
-import sys
-import argparse
+import datetime
 import logging
-import json
+from wikiscout import annotation
+from wikiscout.start import STARTServerException
+from unidecode import unidecode
+import multiprocessing
+from collections import namedtuple
+from pymongo.errors import ConnectionFailure
 
-from wikiscout.annotation import Annotation
-from wikiscout.start import START
-from wikiscout import infobox
-from wikiscout import schemata
+Candidate = namedtuple('Candidate', ['cls', 'attribute', 'sentence', 'object', 'value'])
 
-start = START('nauru.csail.mit.edu')
-attributes = {}
+manager = multiprocessing.Manager()
+results = manager.Queue()
+failQ = manager.Queue()
+dead = manager.Event()
+finished = manager.Event()
 
-def load_attributes(attributes_file):
-    attributes = {}
-    
-    for l in attributes_file:
-        template_attrs = json.loads(l)
-        template = template_attrs['template']
-        for d in template_attrs['attributes']:
-            attribute = d['attribute']
-            raw = d['raw']
-            attributes[(template,attribute)] = raw
-    
-    return attributes
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('/scratch/wikiscout/wikiscout.log')
+logger.addHandler(handler)
 
-def eval_candidate(template,attribute,sentence,value,wiki_title,index):
+
+def process_candidate(c):
+    if dead.is_set():
+        str = "%s\n" % "\t".join([c.cls, c.attribute, c.sentence, c.object, c.value])
+        str = unidecode(unicode(str)).encode('utf8', 'ignore')
+        failQ.put(str)
+        return
+
+    if c.object == c.value:
+        return
+
     try:
-        annotation = Annotation(sentence,wiki_title,template,value).get()
+        a = annotation.annotate(c.sentence, c.object).join_tokens()
+        str = "%s\n" % "\t".join([c.cls, c.attribute, a, c.object, c.sentence, c.value])
+        str = unidecode(unicode(str)).encode('utf8', 'ignore')
+        results.put(str)
+        return
+    except (STARTServerException, ConnectionFailure):
+        if not dead.is_set():
+            dead.set()
+            print "Fatal exception raised while processing:\n%s" % c.__str__()
+        str = "%s\n" % "\t".join([c.cls, c.attribute, c.sentence, c.object, c.value])
+        str = unidecode(unicode(str)).encode('utf8', 'ignore')
+        failQ.put(str)
+        return
     except Exception as e:
-        logging.exception('Could not generate annotation for "%s". Exception:\n%s'%(sentence,e))
-        return None
+        logger.exception(e)
+        return
 
-    if annotation is None:
-        return None
-        
-    try:
-        parseable = start.parseable(annotation)
-    except Exception as e:
-        logging.error('START parse error:\n%s'%e)
-        return None
-        
-    if parseable:
-        wiki_class = infobox.get_class(template)
-        return (annotation,wiki_class)
 
-def generate_schema(index,annotation,wiki_class,attribute,template):
-    if (template,attribute) not in attributes:
-        logging.warn('No raw attributes for attribute "%s" of template "%s"'%(attribute,template))
-        raw_attributes = [attribute]
-    else:
-        raw_attributes = attributes[(template,attribute)]
-    
-    schema = schemata.generate(annotation,index,wiki_class,raw_attributes)
-    return schema
-    
-def main(args, loglevel):
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel, filename='%s/wikiscout.log'%args.output, filemode='w')
+def get_candidates(path):
+    f = open(path)
+    candidates = []
+    for l in f:
+        cls, attribute, sentence, object, value = l.strip().split('\t')
+        candidates.append(Candidate(cls, attribute, sentence, object, value))
+    f.close()
+    return candidates
 
-    schemata_file = open(args.output + '/wikiscout-schemata.lisp','w')
-    log_file = open(args.output + '/wikiscout-output.tsv','w')
-    log_file.write('Index\tTitle\tClass\tAttribute\tValue\tAnnotation\tSentence\n')
-    candidates_file = open(args.candidates)
-    
-    global attributes
-    attributes = load_attributes(open(args.attributes))
-    
-    index = 0
-    total = 0
 
-    for l in candidates_file:
-        template_candidates = json.loads(l)
-        template = template_candidates['infobox_type']
-        
-        for attribute in template_candidates['attributes']:
-            if attribute in ['common_name']:
-                logging.info('Skipping attribute: %s'%attribute)
-                continue
-            
-            candidates = template_candidates['attributes'][attribute]
-            for candidate in candidates:
-                sentence = candidate['sentence']
-                value = candidate['value']
-                wiki_title = candidate['wikiTitle']
-                
-                total += 1
-                result = eval_candidate(template,attribute,sentence,value,wiki_title,index)
-                if result is not None:
-                    annotation,wiki_class = result
-                    schema = generate_schema(index,annotation,wiki_class,attribute,template)
-                    
-                    try:
-                        log_file.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\n'%(index,wiki_title,wiki_class,attribute,value,annotation,sentence))
-                        schemata_file.write(schema)
-                        index += 1
-                    except Exception as e:
-                        logging.error('Could not write schema for article %s, attribute %s and value %s'%(wiki_title,attribute,value))
-        
-    print "====\nWikiScout: Generated %s annotations out of %s candidates\n===="%(index,total)
-        
+def process_results():
+    outf = open('annotations.tsv', 'a')
+    while True:
+        s = results.get(True)
+        if s is None:
+            break
+
+        outf.write('%s' % s)
+        outf.flush()
+    outf.close()
+
+
+def process_fail():
+    failf = open('sentences-remaining.tsv', 'w')
+    while True:
+        s = failQ.get(True)
+        if s is None:
+            break
+
+        failf.write('%s' % s)
+        failf.flush()
+    failf.close()
+
+
+def run():
+    print "Getting candidates ..."
+    candidates = get_candidates('sentences-todo.tsv')
+
+    resultsd = multiprocessing.Process(target=process_results)
+    resultsd.start()
+    faild = multiprocessing.Process(target=process_fail)
+    faild.start()
+    print "Started resultsd and faild"
+
+    start_time = datetime.datetime.now()
+    print "Starting map tasks at %s" % start_time
+    pool.map_async(process_candidate, candidates)
+    pool.close()
+    pool.join()
+
+    results.put(None)
+    failQ.put(None)
+
+    resultsd.join()
+    faild.join()
+    print "Total runtime: %s" % (datetime.datetime.now() - start_time)
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description = "Generates a schemata file of Wikiscout annotations",
-        epilog = "As an alternative to the commandline, params can be placed in a file, one per line, and specified on the commandline like '%(prog)s @params.conf'.",
-        fromfile_prefix_chars = '@' )
-
-    parser.add_argument(
-        "attributes",
-        help = "the attributes file",
-        metavar = "attributes")
-
-    parser.add_argument(
-        "candidates",
-        help = "the file with extracted sentences",
-        metavar = "candidates")
-
-    parser.add_argument(
-        "output",
-        help = "the destination directory",
-        metavar = "output")
-
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        help="increase output verbosity",
-        action="store_true")
-
-    parser.add_argument(
-        "-q",
-        "--quiet",
-        help="decrease output verbosity",
-        action="store_true")
-
-    args = parser.parse_args()
-
-    loglevel = logging.INFO
-    if args.verbose:
-        loglevel = logging.DEBUG
-    if args.quiet:
-        loglevel = logging.WARN
-    
-    main(args, loglevel)
-            
+    pool = multiprocessing.Pool(12)
+    run()
