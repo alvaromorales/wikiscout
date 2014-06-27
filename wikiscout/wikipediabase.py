15f732536e01c220ac8516123a54809388f728a9
alvarom <alvarom@f1b2aeda-bd07-0410-ad1a-e603d8ac5cf8>
2014-06-27 03:58:18 +0000

    Refactor to module
    git-svn-id: svn+ssh://svn.csail.mit.edu/afs/csail.mit.edu/group/infolab/svnroot/wikiscout@1883 f1b2aeda-bd07-0410-ad1a-e603d8ac5cf8

--- wikiscout/wikipediabase.py
+++ wikiscout/wikipediabase.py
@@ -1,43 +1,48 @@
-from pyparsing import OneOrMore, nestedExpr
 import re
 import telnetlib
 
-class WikipediaBase:
-    def __init__(self,host='localhost'):
-        self.host = host
-        self.scheme_parser = OneOrMore(nestedExpr())
-        self.connect()
-    
-    def connect(self):
-        # open a new telnet connection to WikipediaBase
-        try:
-            self.telnet = telnetlib.Telnet(self.host,8023)
-        except:
-            print "Could not connect to WikipediaBase host %s"%self.host
-            raise
-    
-    def get_classes(self,s):
-        self.connect()
-        self.telnet.write('(get-classes "%s")\n'%s)
-        response = self.telnet.read_all()
 
-        try:
-            parsed_response = self.parse_get_classes_response(response)
-        except:
-            raise Exception('WikipediaBase could not parse: %s'%self.sentence)
+class WikipediaBaseConnectionException(Exception):
+    pass
 
-        self.close()
-        return parsed_response
-    
-    def parse_get_classes_response(self,response):
-        if response.strip() == '#f':
-            return []
 
-        response = re.sub(r'<!---.*?\"','\"',response)
-        response = re.sub(r'\n','',response)
+class WikipediaBaseParseException(Exception):
+    pass
 
-        return map(lambda c: eval(c),response.strip()[1:-1].split(' '))
 
-    def close(self):
-        self.telnet.close()
+class WikipediaBaseException(Exception):
+    pass
 
+
+def _connect(host='localhost'):
+    try:
+        conn = telnetlib.Telnet(host, 8023)
+        return conn
+    except:
+        raise WikipediaBaseConnectionException(
+            'Could not connect to WikipediaBase host %s' % host)
+
+
+def get_classes(symbol, host='localhost'):
+    conn = _connect(host)
+    conn.write('(get-classes "%s")\n' % symbol)
+    response = conn.read_all()
+    conn.close()
+
+    try:
+        parsed_response = _parse_get_classes_response(response)
+    except:
+        raise WikipediaBaseConnectionException(
+            'WikipediaBase could not parse: %s' % symbol)
+
+    return parsed_response
+
+
+def _parse_get_classes_response(response):
+    if response.strip() == '#f':
+        return []
+
+    response = re.sub(r'<!---.*?\"', '\"', response)
+    response = re.sub(r'\n', '', response)
+
+    return map(lambda c: eval(c), response.strip()[1:-1].split(' '))
