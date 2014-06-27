96f6278b1c8908055c1dba2edba55ed1af672608
alvarom <alvarom@f1b2aeda-bd07-0410-ad1a-e603d8ac5cf8>
2014-06-27 03:58:09 +0000

    Refactored START services into modules
    git-svn-id: svn+ssh://svn.csail.mit.edu/afs/csail.mit.edu/group/infolab/svnroot/wikiscout@1867 f1b2aeda-bd07-0410-ad1a-e603d8ac5cf8

--- wikiscout/start.py
+++ wikiscout/start.py
@@ -1,70 +1,77 @@
-from pyparsing import OneOrMore, nestedExpr
-from xml.sax.saxutils import escape
 import telnetlib
 import xmltodict
 import socket
 import re
 import json
 
-class START:
-    def __init__(self,host='localhost'):
-        self.host = host
+_servers = {
+    'guest': 8001,
+    'ailab': 8002,
+}
 
-    def connect(self):
-        # open a new telnet connection to START
-        try:
-            self.telnet = telnetlib.Telnet(self.host,8001)
-            self.remote_address = socket.gethostbyname(socket.gethostname())
-        except:
-            raise Exception("Could not connect to START host %s"%self.host)
-    
-    def format_request(self,query,action,kb='yes',machine='nauru.csail.mit.edu',server='guest'):
-        request = '(:REMOTE-ADDRESS "%s" :VB "normal" :QUERY %s :USER_AGENT "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36" :TE "XML" :KB "%s" :REFERRER "http://nauru.csail.mit.edu/wikiscout" :DE "no" :ACTION "%s" :FG "yes" :SERVER "%s" :QE "HTML" :EQ "history" :QI "qsymbols" :MACHINE "%s" :HTTP_HOST "groups.csail.mit.edu")'%(self.remote_address,json.dumps(query),kb,action,server,machine)
-        return request
 
-    def parseable(self,sentence):
-        return self.parse(sentence) is not None
-    
-    def parse(self,sentence):
-        self.connect()
-        request = self.format_request(sentence,'parse',kb='no')
-        self.telnet.write(request)
-        response = self.telnet.read_all().strip()
-        
-        if response.find('===> NULL') != -1:
-            return None
+class STARTConnectionException(Exception):
+    pass
 
-        try:
-            parsed = xmltodict.parse(response)
-        except:
-            raise Exception("Could not parse:\n%s"%response)
 
-        if 'interaction' in parsed and 'query' in parsed['interaction']:
-            if 'exception' not in parsed['interaction']['query'] and 'reply' in parsed['interaction']['query']:
-                return parsed['interaction']['query']
-        
+def _connect(host='localhost', server='guest'):
+    port = _servers[server]
+    try:
+        telnet = telnetlib.Telnet(host, port)
+        remote_address = socket.gethostbyname(socket.gethostname())
+        return (telnet, remote_address)
+    except:
+        raise STARTConnectionException("Could not connect to START host %s" % host)
+
+
+def _format_request(query, action, remote_address, kb='yes', machine='malta.csail.mit.edu', server='guest'):
+    request = '(:REMOTE-ADDRESS "%s" :VB "normal" :QUERY %s :USER_AGENT "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36" :TE "XML" :KB "%s" :REFERRER "http://malta.csail.mit.edu/wikiscout" :DE "no" :ACTION "%s" :FG "yes" :SERVER "%s" :QE "HTML" :EQ "history" :QI "qsymbols" :MACHINE "%s" :HTTP_HOST "groups.csail.mit.edu")' % (remote_address, json.dumps(query), kb, action, server, machine)
+    return request
+
+
+def parse(sentence, host='localhost', server='guest'):
+    telnet, remote_address = _connect(host, server)
+    request = _format_request(sentence, 'parse', remote_address, kb='no')
+    telnet.write(request)
+    response = telnet.read_all().strip()
+    telnet.close()
+
+    if response.find('===> NULL') != -1:
         return None
 
-    def ask(self,question):
-        self.connect()
-        question = escape(question)
-        request = self.format_request(question,'askstart')
-        self.telnet.write(request)
-        response = self.telnet.read_all().strip()
-        self.close()
-        response = response.replace('<EM>','')
-        response = response.replace('</EM>','')
-        response = response.replace('<HR>','')
-        response = response.replace('</HR>','')
-        response = re.sub(r'<img.*?>', '',response)
-        try:
-            parsed = xmltodict.parse(response)
-        except:
-            raise Exception("Could not parse:\n%s"%response)
+    try:
+        parsed = xmltodict.parse(response)
+    except:
+        raise Exception("Could not parse:\n%s" % response)
+
+    if 'interaction' in parsed and 'query' in parsed['interaction']:
+        if 'exception' not in parsed['interaction']['query'] and 'reply' in parsed['interaction']['query']:
+            return parsed['interaction']['query']
+
+    return None
+
+
+def parseable(self,sentence):
+    return self.parse(sentence) is not None
+
+
+def ask(question, host='localhost', server='guest'):
+    telnet, remote_address = _connect(host, server)
+    request = _format_request(question, 'parse', remote_address, kb='no')
+    telnet.write(request)
+    response = telnet.read_all().strip()
+    telnet.close()
+
+    response = response.replace('<EM>', '')
+    response = response.replace('</EM>', '')
+    response = response.replace('<HR>', '')
+    response = response.replace('</HR>', '')
+    response = re.sub(r'<img.*?>',  '', response)
+
+    try:
+        parsed = xmltodict.parse(response)
+    except:
+        raise Exception("Could not parse:\n%s" % response)
         if 'interaction' in parsed and 'query' in parsed['interaction']:
             return parsed['interaction']['query']
         return None
-
-    def close(self):
-        self.telnet.close()
-
--- wikiscout/wikikb.py
+++ wikiscout/wikikb.py
@@ -1,83 +1,93 @@
-import re
-import mwparserfromhell
-import logging
-from dewiki.parser import Parser as DewikiParser
 from pymongo import MongoClient
 import infobox
 
-class WikiKB:
-    def __init__(self,host='localhost'):
-        self.client = MongoClient(host, 27017)
-        self.db = self.client.wiki        
-        self.wikipedia = self.db.wikipedia
+def _connect(host='localhost'):
+    """Returns a client and db connection to a MongoDB host with WikiKB
+    data."""
+    client = MongoClient(host,27017)
+    db = client.wiki
+    return (client,db)
 
-    def get_article(self,title):
-        article = self.wikipedia.find_one({'title':title})
-        
-        if article is None:
-            return None
-        
-        while article is not None and 'redirect' in article:
-            article = self.wikipedia.find_one({'title': article['redirect']})
-        
-        return article
 
-    def get(self,wiki_class,title,attribute):
-        try:
-            article = self.wikipedia.find_one({'title':title})
-            article_infobox = self.get_infobox(article)
-            if article_infobox is not None and article_infobox.name == wiki_class:
-                items = article_infobox.items
-                if attribute in items:
-                    value = items[attribute]
-                    return value
-                return None
-        except Exception as e:
-            logging.exception(e)
-            return None
+def get_article(title, lang='en'):
+    """Gets an article.
 
-    def get_infobox(self,article):
-        if article is None or 'infobox' not in article:
-            return None
-        try:
-            infobox_template = infobox.get_class(article['infobox']['name'])
-            description = article['infobox']['description']
-            
-            if infobox_template != '' and description != []:
-                article_infobox = infobox.Infobox(article['infobox'])
-                return article_infobox
-        except Exception as e:
-            logging.exception(e)
-            return None
+    Args:
+      title (str): The title of the article.
+      lang (str): The language of the article. Defaults to 'en'
+    """
+    client, db = _connect()
 
-    def get_attributes(self,wiki_class,title):
-        try:
-            article = self.get_article(title)
-            article_infobox = self.get_infobox(article)
-            if article_infobox is not None and article_infobox.name == wiki_class:
-                attributes = article_infobox.get_all_attributes()
-                return attributes
-            else:
-                return []
-        except Exception as e:
-            logging.exception(e)
-            return []
+    if lang == 'en':
+        article = db.wikipedia.find_one({'title': title})
+    elif lang == 'simple':
+        article = db.simplewikipedia.find_one({'title': title})
+    else:
+        return None
 
-    def get_classes(self,title):
-        article = self.get_article(title)
+    while article is not None and 'redirect' in article:
+        article = db.wikipedia.find_one({'title': article['redirect']})
 
-        if article is None:
-            return []
-        else:
-            wiki_classes = ['wikipedia-term','wikipedia-paragraphs']
-            if 'infobox' not in article:
-                return wiki_classes
+    return article
 
-            infobox_template = article['infobox']['name']
 
-            if infobox_template is not None and infobox_template != '':
-                wiki_class = infobox.get_class(infobox_template)
-                wiki_classes.append(wiki_class)
-                return wiki_classes
-            
-            return wiki_classes
+def get_infobox(title, lang='en'):
+    """Gets an article's infobox.
+
+    Args:
+      title (str): The title of the article.
+      lang (str): The language of the article. Defaults to 'en'.
+    """
+    article = get_article(title,lang)
+
+    if article is None or 'infobox' not in article:
+        return None
+
+    article_infobox = infobox.Infobox(article['infobox'])
+    return article_infobox
+
+
+def get(cls,title,attribute):
+    """Gets the value of an infobox attribute.
+
+    Args:
+      cls (str): The WikipediaBase class.
+      title (str): The title of the article.
+      attribute (str): The infobox attribute.
+    """
+    article_infobox = get_infobox(title)
+    if article_infobox and article_infobox.name == cls:
+        items = article_infobox.items
+        if attribute in items:
+            return items[attribute]
+    return None
+
+
+def get_attributes(cls,title):
+    """Gets the attributes in an article's infobox.
+
+    Args:
+      cls (str): The WikipediaBase class.
+      title (str): The title of the article.
+    """
+    article_infobox = get_infobox(title)
+    if article_infobox and article_infobox.name == cls:
+        return article_infobox.get_all_attributes()
+    return []
+
+
+def get_classes(title):
+    """Gets the WikipediaBase classes of an article.
+
+    Args:
+      title (str): The article title.
+    """
+    article = get_article(title)
+    
+    if article:
+        wiki_classes = ['wikipedia-term','wikipedia-paragraphs']
+        article_infobox = get_infobox(title)
+        if article_infobox:
+            wiki_classes.append(article_infobox.name)
+        return wiki_classes
+    return []
