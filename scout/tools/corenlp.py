c7b2f9bb1a5e0dd5ead7942bd424e4ba3401dfbe
alvarom <alvarom@f1b2aeda-bd07-0410-ad1a-e603d8ac5cf8>
2014-06-27 03:57:59 +0000

    Added NER substitution to annotations
    git-svn-id: svn+ssh://svn.csail.mit.edu/afs/csail.mit.edu/group/infolab/svnroot/wikiscout@1852 f1b2aeda-bd07-0410-ad1a-e603d8ac5cf8

---
+++ scout/tools/corenlp.py
@@ -0,0 +1,53 @@
+from string import punctuation
+import jsonrpclib
+from simplejson import loads
+from nltk.corpus import stopwords
+
+class CoreNLP:
+    def __init__(self,host='localhost'):
+        self.server = jsonrpclib.Server("http://%s:6888"%host)
+        self.stopwords = set(stopwords.words('english'))
+        self.punctuation = set(punctuation)
+        self.tagmap = {'LOCATION': 'any-wikipedia-location',
+                       'TIME': 'any-time',
+                       'PERSON': 'any-wikipedia-person',
+                       'ORGANIZATION': 'any-wikipedia-organization',
+                       'MONEY': 'any-money',
+                       'PERCENT': 'any-percent',
+                       'DATE': 'any-date'
+                       }
+
+    def parse(self,text):
+        return loads(self.server.parse(text))
+
+    def ner(self,text):
+        parse = self.parse(text)
+
+        if parse is None:
+            return
+
+        new_text = []
+        
+        for sentence in parse['sentences']:
+            if 'words' not in sentence:
+                continue
+
+            for word,attributes in sentence['words']:
+                if word in self.stopwords or word.startswith('any-wikipedia-'):
+                    new_text.append(word)
+                    continue
+
+                if 'NamedEntityTag' in attributes:
+                    tag = attributes['NamedEntityTag']
+                    if tag in self.tagmap:
+                        if len(new_text) > 0:
+                            last_word = new_text[-1]
+                            if last_word == self.tagmap[tag]:
+                                continue
+                        new_text.append(self.tagmap[tag])
+                    else:
+                        new_text.append(word)
+                else:
+                    new_text.append(word)
+        
+        return ''.join(w if set(w) <= self.punctuation or w == "'s" else ' '+w for w in new_text).lstrip()
