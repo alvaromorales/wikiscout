ece004e41c5f1a06853e00be8074a027096912df
alvarom <alvarom@f1b2aeda-bd07-0410-ad1a-e603d8ac5cf8>
2014-06-27 03:58:01 +0000

    Fixed tests
    git-svn-id: svn+ssh://svn.csail.mit.edu/afs/csail.mit.edu/group/infolab/svnroot/wikiscout@1855 f1b2aeda-bd07-0410-ad1a-e603d8ac5cf8

--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -3,7 +3,7 @@ from scout.tools.annotation import Annotation
 
 class TestGenerateSymbol(unittest.TestCase):
     def test_infobox_template(self):
-        self.assertEqual(Annotation.generate_symbol('infobox_officeholder'),'any-wikipedia-officeholder')
+        self.assertEqual(Annotation.generate_symbol('infobox_president'),'any-wikipedia-president')
 
     def test_wikipedia_symbol(self):
         self.assertEqual(Annotation.generate_symbol('Wikipedia-Person'), 'any-wikipedia-person')
@@ -18,27 +18,27 @@ class TestSubTitle(unittest.TestCase):
     def test_simple(self):
         sentence = "Bill Clinton married Hillary Clinton in 1975."
         wiki_title = "Bill_Clinton"
-        template = "infobox_officeholder"
+        template = "infobox_president"
         value = "Hillary Clinton"
 
         annotation = Annotation(sentence,wiki_title,template,value)
 
-        self.assertEqual(annotation.sub_title(),"any-wikipedia-officeholder married Hillary Clinton in 1975.")
+        self.assertEqual(annotation.sub_title(),"any-wikipedia-president married Hillary Clinton in 1975.")
 
     def test_possesive(self):
         sentence = "Bill Clinton's wife is Hillary Clinton."
         wiki_title = "Bill_Clinton"
-        template = "infobox_officeholder"
+        template = "infobox_president"
         value = "Hillary Clinton"
 
         annotation = Annotation(sentence,wiki_title,template,value)
 
-        self.assertEqual(annotation.sub_title(),"any-wikipedia-officeholder's wife is Hillary Clinton.")
+        self.assertEqual(annotation.sub_title(),"any-wikipedia-president's wife is Hillary Clinton.")
     
     def test_no_title(self):
         sentence = "Hillary Rodham Clinton was born in Chicago."
         wiki_title = "Bill_Clinton"
-        template = "infobox_officeholder"
+        template = "infobox_president"
         value = "Chicago"
 
         annotation = Annotation(sentence,wiki_title,template,value)
@@ -49,27 +49,27 @@ class TestSubPronoun(unittest.TestCase):
     def test_possesive(self):
         sentence = "He was born in Arkansas."
         wiki_title = "Bill_Clinton"
-        template = "infobox_officeholder"
+        template = "infobox_president"
         value = "Arkansas"
         
         annotation = Annotation(sentence,wiki_title,template,value)
 
-        self.assertEquals(annotation.sub_pronoun(),"any-wikipedia-officeholder was born in Arkansas.")
+        self.assertEquals(annotation.sub_pronoun(),"any-wikipedia-president was born in Arkansas.")
 
     def test_subjective(self):
         sentence = "His father was William Jefferson Blythe, Jr."
         wiki_title = "Bill_Clinton"
-        template = "infobox_officeholder"
+        template = "infobox_president"
         value = "William Jefferson Blythe, Jr"
 
         annotation = Annotation(sentence,wiki_title,template,value)
 
-        self.assertEquals(annotation.sub_pronoun(),"any-wikipedia-officeholder's father was William Jefferson Blythe, Jr.")
+        self.assertEquals(annotation.sub_pronoun(),"any-wikipedia-president's father was William Jefferson Blythe, Jr.")
         
     def test_no_pronoun(self):
         sentence = "Georgetown was his alma mater."
         wiki_title = "Bill_Clinton"
-        template = "infobox_officeholder"
+        template = "infobox_president"
         value = "Georgetown"
         
         annotation = Annotation(sentence,wiki_title,template,value)
@@ -80,29 +80,29 @@ class TestSubHighlights(unittest.TestCase):
     def test_longest_highlight(self):
         sentence = "William Jefferson Clinton was a US president."
         wiki_title = "Bill_Clinton"
-        template = "infobox_officeholder"
+        template = "infobox_president"
         value = "US president"
         highlights = ['William Jefferson Clinton', 'William Jefferson Blythe III', 'Bill Clinton', 'My Life','Bill','Clinton']
 
         annotation = Annotation(sentence,wiki_title,template,value,highlights=highlights)
         
-        self.assertEquals(annotation.sub_highlights(),"any-wikipedia-officeholder was a US president.")
+        self.assertEquals(annotation.sub_highlights(),"any-wikipedia-president was a US president.")
 
     def test_possesive(self):
         sentence = "Clinton's nickname is Bill."
         wiki_title = "Bill_Clinton"
-        template = "infobox_officeholder"
+        template = "infobox_president"
         value = "Bill"
         highlights = ['William Jefferson Clinton', 'William Jefferson Blythe III', 'Bill Clinton', 'My Life','Bill','Clinton']
 
         annotation = Annotation(sentence,wiki_title,template,value,highlights=highlights)
         
-        self.assertEquals(annotation.sub_highlights(),"any-wikipedia-officeholder's nickname is Bill.")
+        self.assertEquals(annotation.sub_highlights(),"any-wikipedia-president's nickname is Bill.")
         
     def test_no_highlights(self):
         sentence = "Barack Obama is the president of the US."
         wiki_title = "Bill_Clinton"
-        template = "infobox_officeholder"
+        template = "infobox_president"
         value = "president of the US"
         highlights = ['William Jefferson Clinton', 'William Jefferson Blythe III', 'Bill Clinton', 'My Life','Bill','Clinton']
 
@@ -114,11 +114,11 @@ class TestSubWikipediaBaseSymbols(unittest.TestCase):
     def test(self):
         sentence = "William Clinton was the president of the US."
         wiki_title = "Bill_Clinton"
-        template = "infobox_officeholder"
+        template = "infobox_president"
         value = "president of the US"
         
         annotation = Annotation(sentence,wiki_title,template,value)        
-        self.assertEquals(annotation.sub_wb_subject(),"any-wikipedia-officeholder was the president of the US.")
+        self.assertEquals(annotation.sub_wb_subject(),"any-wikipedia-president was the president of the US.")
 
 #class TestAnnotation(unitest.TestCase):
 #    pass
